f95d69448b8bcdd94abb1c44c545ead1953b7668
rmsci <74026422+rmsci@users.noreply.github.com>
2020-11-05 18:13:49 -0500

    Update c.js

--- c.js
+++ c.js
@@ -1,1 +1,1 @@
-console.log('Hello Dr Hamman!\nI would like to teach on Tuesday evenings.\nWill a 6-9pm class work?');
+console.log('Hello Dr. Hamman!\nI would like to teach on Tuesday evenings.\nWill a 6-9pm class work?');
