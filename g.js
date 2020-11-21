6d9d6b602fc3d7d0fc7068ad6ef39e39b01be867
realmadsci <71108352+realmadsci@users.noreply.github.com>
2020-11-21 14:43:42 -0500

    Create g.js

---
+++ g.js
@@ -0,0 +1,17 @@
+// Inject CSS and font into the HEAD portion
+/*
+<HEAD>
+<link rel="stylesheet" href="https://realmadsci.github.io/j/glitch.css">
+<link href="https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap" rel="stylesheet">
+</HEAD>
+*/
+
+// Inject body overlay and text
+/*
+<div class="realmadoverlay">
+    <div class="realmadglitch" data-text="realmadsci">realmadsci</div>
+    <div class="realmadglitch" data-text="was">was</div>
+    <div class="realmadglitch" data-text="here">here</div>
+  </div>
+<BODY>
+*/
