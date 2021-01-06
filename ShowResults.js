21d929658aa65e2e2098eac244a6d33bef82b5af
rmsci <74026422+rmsci@users.noreply.github.com>
2021-01-05 20:22:49 -0500

    Update ShowResults.js

--- ShowResults.js
+++ ShowResults.js
@@ -39,7 +39,7 @@ function makeGlitchWord(word) {
 
 let overlay = makeDivWithClass('realmadoverlay');
 overlay.style.opacity = 0.8; // Make it darker
-let w = ['Welcome', 'to', 'CS4310'];
+let w = ['Welcome', 'to', 'CY4310'];
 w.forEach(function(word) {
     overlay.appendChild(makeGlitchWord(word));
 });
